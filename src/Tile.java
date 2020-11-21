481267eaea7324bcc1e8fbf268eb42674e8b8b85
K1chu <74359039+K1chu@users.noreply.github.com>
2020-11-21 14:41:17 +0000

    Add files via upload

--- src/Tile.java
+++ src/Tile.java
@@ -1,2 +1,20 @@
-public class Tile {
+
+public abstract class Tile 
+{
+	String tileType;
+	
+	protected Tile(String tileType)
+	{
+		this.tileType = tileType;
+	}
+	
+	public void setTileType (String tileType)
+	{
+		this.tileType = tileType;
+	}
+	
+	public String getTileType()
+	{
+		return tileType;
+	}
 }
