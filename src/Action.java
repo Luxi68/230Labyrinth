5213cf896ce470f7193217cc6862c45674ff73c3
K1chu <74359039+K1chu@users.noreply.github.com>
2020-11-23 16:18:14 +0000

    Add files via upload

--- src/Action.java
+++ src/Action.java
@@ -4,9 +4,17 @@ public abstract class Action extends Tile
 	public Action(String tileType)
 	{
 		super(tileType);
+		if (tileType.equalsIgnoreCase("fire"))
+		{
+			Type = fire;
+		}
+	}
+	enum Type
+	{
+		fire, ice, doubleMove, backTrack;
 	}
 	
-	public void Use()
+	public void use()
 	{
 		
 	}
