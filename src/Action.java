313ac3acadc0adb580f6801107641af137529bf4
nouaxs <74322389+nouaxs@users.noreply.github.com>
2020-11-23 16:48:50 +0000

    Add files via upload

--- src/Action.java
+++ src/Action.java
@@ -1,21 +1,39 @@
 
-public abstract class Action extends Tile 
-{
-	public Action(String tileType)
-	{
+public class Action extends Tile {
+	
+	private Type actionType;
+	
+	public Action(String tileType) {
 		super(tileType);
-		if (tileType.equalsIgnoreCase("fire"))
-		{
-			Type = fire;
+		
+		if (tileType.equalsIgnoreCase("fire")) {
+			this.actionType = Type.fire;
+		} else if (tileType.equalsIgnoreCase("ice")) {
+			this.actionType = Type.ice;
+		} else if (tileType.equalsIgnoreCase("doubleMove")) {
+			this.actionType = Type.doubleMove;
+		} else if (tileType.equalsIgnoreCase("backTrack")) {
+			this.actionType = Type.backTrack;
 		}
 	}
-	enum Type
-	{
+	
+	enum Type {
 		fire, ice, doubleMove, backTrack;
 	}
 	
-	public void use()
-	{
-		
+	public void useFireIce(Floor tile) {
+		if (actionType.equals(Type.fire)) {
+			tile.setIsFire(true);
+		} else if (actionType.equals(Type.ice)) {
+			tile.setIsIce(true);
+		}
+	}
+	
+	public void useBackDouble(Player player) {
+		if (actionType.equals(Type.doubleMove)) {
+			//player.doubleMove();
+		} else if (actionType.equals(Type.backTrack)){
+			//player.backTrack();
+		}
 	}
 }
