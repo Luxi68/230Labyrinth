450cfd8080f6f28aef57b04875031e62ecef521f
nouaxs <74322389+nouaxs@users.noreply.github.com>
2020-11-26 00:17:24 +0000

    Add files via upload

--- src/Board.java
+++ src/Board.java
@@ -1,8 +1,8 @@
 package game;
 
 /**
- * <P>Purpose: Setting out tiles by collaborating with Floor in order to create the game board. 
- * It is also responsible for managing the tiles movements</p>
+* <P>Purpose: Setting out tiles by collaborating with Floor in order to create the game board. 
+* It is also responsible for managing the tiles movements</p>
 * <P>File name: Board.java </p> 
 * @author Nouran and Gordon
 * <P>Created: 16/11/2020
@@ -17,16 +17,20 @@ public class Board{
 	
 	/**
 	 * Constructor used to create a new instance of Board.
+	 * @param height The height of the board (i.e. how many tiles it will hold vertically)
+	 * @param length The length of the board (i.e. how many tiles it will hold horizontally)
 	 */
 	public Board (int height, int length) {
-		this.board = new Floor[height][length];//do we need -1?
+		this.board = new Floor[height-1][length-1];//do we need -1?
 		this.length = length;
+		//add tiles setup...
+		//add players arraylist and their starting points...
 	}
 	/*
 	 * insertFloor inserts a Floor tile to the board and shifts relevant tiles
-	 * @param p The profile that needs to get inserted
-	 * @param temp, the node we're using to move along the tree
-	 * 
+	 * @param insert The Floor tile that needs to be inserted
+	 * @param The y coordinate of the row we need to insert the tile into
+	 
 	public void insertFloor(Floor insert, int x, int y, button?) {
 		//change it to switch statement!!
 		if (button says insert from left) {
@@ -41,14 +45,28 @@ public class Board{
 			
 	}*/
 	
+	public Floor getTileAt(int x, int y) {
+		return board[x][y];
+	}
+	
+	public void insertTileAt(int x, int y, Floor insert) {
+		board[x][y] = insert;
+	}
+	
+	
+	/**
+	 * insertFromLeft inserts a Floor tile to the board and shifts relevant tiles from the left side
+	 * @param insert The Floor tile that needs to be inserted
+	 * @param y The y coordinate of the row we need to insert the tile to
+	 */
 	public void insertFromLeft(Floor insert,int y) {
 		//make sure we can insert the tile
 		int reachedEnd = 0;
 		Floor curFloor = null; // current floor
 		int xCurFloor = length; //the x of the current floor
-		// insert board[length][y] in silk bag
+		// insert board[length][y] in silk bag.. need to access silkbag somehow
 		board[length][y] = null;
-			while (reachedEnd != length) { //again we need to decide if length starts from 0 or 1
+			while (reachedEnd != length) { //maybe change into for loop?
 				curFloor = board[xCurFloor - 1][y];
 				board[xCurFloor][y] = curFloor;
 				xCurFloor --;
@@ -58,6 +76,11 @@ public class Board{
 		//we need to add conditions to deal with fixed tile n all
 	}
 	
+	/**
+	 * insertFromRight inserts a Floor tile to the board and shifts relevant tiles from the right side
+	 * @param insert The Floor tile that needs to be inserted
+	 * @param y The y coordinate of the row we need to insert the tile to
+	 */
 	public void insertFromRight(Floor insert, int y) {
 		//make sure we can insert the tile
 		int reachedEnd = 0;
@@ -65,7 +88,7 @@ public class Board{
 		int xCurFloor = 0; //the x of the current floor
 		// insert board[length][y] in silk bag
 		board[0][y] = null;
-			while (reachedEnd != length) { //again we need to decide if length starts from 0 or 1
+			while (reachedEnd != length) { //maybe change into for loop?
 				curFloor = board[xCurFloor + 1][y];
 				board[xCurFloor][y] = curFloor;
 				xCurFloor ++;
@@ -76,8 +99,11 @@ public class Board{
 	}
 	
 	
-	
-	
+	/**
+	 * insertFromTop inserts a Floor tile to the board and shifts relevant tiles from the top of the board
+	 * @param insert The Floor tile that needs to be inserted
+	 * @param x The x coordinate of the column we need to insert the tile to
+	 */
 	public void insertFromTop(Floor insert, int x) {
 		//make sure we can insert the tile
 		int reachedEnd = 0;
@@ -85,7 +111,7 @@ public class Board{
 		int yCurFloor = height; //the y of the current floor
 		// insert board[x][height] in silk bag
 		board[x][height] = null;
-			while (reachedEnd != height) { //again we need to decide if height starts from 0 or 1
+			while (reachedEnd != height) { //maybe change into for loop?
 				curFloor = board[x][yCurFloor - 1];
 				board[x][yCurFloor] = curFloor;
 				yCurFloor --;
@@ -95,24 +121,106 @@ public class Board{
 		//we need to add conditions to deal with fixed tile n all
 	}
 	
-	
-	public void insertFromBottom(Floor insert, int x) {
-		//make sure we can insert the tile
-		int reachedEnd = 0;
-		Floor curFloor = null; // current floor
-		int yCurFloor = 0; //the y of the current floor
-		// insert board[x][height] in silk bag
-		board[x][0] = null;
-			while (reachedEnd != height) { //again we need to decide if height starts from 0 or 1
-				curFloor = board[x][yCurFloor + 1];
-				board[x][yCurFloor] = curFloor;
-				yCurFloor ++;
-				reachedEnd ++;
-			}
-		board [x][0] = insert;
+	/**
+	 * insertFromBottom inserts a Floor tile to the board and shifts relevant tiles from the bottom of the board
+	 * @param insert The Floor tile that needs to be inserted
+	 * @param x The x coordinate of the column we need to insert the tile to
+	 */
+	public void insertFromBottom(Floor insert, int x) { //need to add the exception
+		if (checkIfColumnMovable(x, insert)) { 
+			int reachedEnd = 0;
+			Floor curFloor = null; // current floor
+			int yCurFloor = 0; //the y of the current floor
+			// insert board[x][height] in silk bag
+			board[x][0] = null;
+				while (reachedEnd != height) { //maybe change into for loop?
+					curFloor = board[x][yCurFloor + 1];
+					board[x][yCurFloor] = curFloor;
+					yCurFloor ++;
+					reachedEnd ++;
+				}
+				board [x][0] = insert;
+		} else {
+		}
 		//we need to add conditions to deal with fixed tile n all
 	}
 	
+	/**
+	 * checkIfRowMovable checks whether it is possible or not to insert a tile into this row
+	 * @param insert The Floor tile that needs to be inserted
+	 * @param curTile
+	 */
+	public boolean checkIfRowMovable(int y, Floor curTile) {//x starts at 0
+		for (int i=0; i<= length-1; i++) {
+			if ( i <= length-1) {
+				if (curTile.getIsIce() == true || curTile.getIsFixed() == true) {
+					return false;
+				} else {
+					curTile = board[i][y];
+				}
+			}
+		}
+		return true;
+	}
 	
+	public boolean checkIfColumnMovable(int x, Floor curTile) {//x starts at 0
+		for (int i=0; i<= height-1; i++) {
+			if ( i <= height-1) {
+				if (curTile.getIsIce() == true || curTile.getIsFixed() == true) {
+					return false;
+				} else {
+					curTile = board[x][i];
+				}
+			}
+		}
+		return true;
+	}
+	/*
+	public void useActionTiles(int x, int y) {
+		//useFireTile(board[x][y]),
+	}
+	*/
+	
+	public Floor[] getSurroundingTiles(Floor inflictedTile) {
+		Floor affectedTiles[];
+		
+		if (inflictedTile.equals(board[0][0])) {
+			affectedTiles = new Floor[4];
+			affectedTiles[0] = board[0][0];
+			affectedTiles[1] = board[1][0];
+			affectedTiles[2] = board[0][1];
+			affectedTiles[3] = board[1][1];
+			
+		} else if (inflictedTile.equals(board[length -1][0])) {
+			affectedTiles = new Floor[4];
+			affectedTiles[0] = board[length-1][0];
+			affectedTiles[1] = board[length-2][0];
+			affectedTiles[2] = board[length-1][1];
+			affectedTiles[3] = board[length-2][1];
+			
+		} else if (inflictedTile.equals(board[0][height -1])) {
+			affectedTiles = new Floor[4];
+			affectedTiles[0] = board[0][height-1];
+			affectedTiles[1] = board[0][height-2];
+			affectedTiles[2] = board[1][height-1];
+			affectedTiles[3] = board[1][height-2];
+			
+		} else if (inflictedTile.equals(board[length -1][height -1])) {
+			affectedTiles = new Floor[4];
+			affectedTiles[0] = board[length-1][height-1];
+			affectedTiles[1] = board[length-1][height-2];
+			affectedTiles[2] = board[length-2][height-1];
+			affectedTiles[3] = board[length-2][height-2];
+			
+		// still need to make conditions for top/bottom/side rows
+		} else { // all 3x3 square is affected
+			affectedTiles = new Floor[9];
+			
+		}
+		return affectedTiles;
+	}		
 }
+	
+	
+
 	
