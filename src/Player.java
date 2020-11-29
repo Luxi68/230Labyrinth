baed669604e4d84ecd50fb9d1e95c02733b22a36
Lucy <lucy.j.hu@gmail.com>
2020-11-29 17:16:45 +0000

    Added Player class to help test javafx

---
+++ src/Player.java
@@ -0,0 +1,17 @@
+public class Player { // TODO - finish class
+	private String name;
+	private int order;
+
+	public Player(String name, int order) {
+		this.name = name;
+		this.order = order;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getOrder() {
+		return order;
+	}
+}
