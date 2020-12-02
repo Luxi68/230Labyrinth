a765a4df634ed37aeb2a3177e7fab6232a0b9df9
nouaxs <74322389+nouaxs@users.noreply.github.com>
2020-12-02 14:23:49 +0000

    Finished player class

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 /**
  * Class that represents the players and their tokens within the game
  *
- * @author Junjie
+ * @author Nouran, Chris, Junjie.
  */
 public class Player {
 	private final String NAME;
@@ -23,16 +23,17 @@ public class Player {
 	private Board board;
 	private int[] lastPosiX = new int[3];
 	private int[] lastPosiY = new int[3];
+	private boolean backtracked;
 
 	// TODO - implement backtrack + bool check
 
 
 	/**
 	 * Constructor to create new player object
-	 *
+	 * @param name  - In-game name of the player token
 	 * @param image - Image representing the player token
 	 */
-	public Player(Image image, String hexColour, int xStart, int yStart, Board board, Profile profile) {
+	public Player(String name, Image image, String hexColour, int xStart, int yStart, Board board, Profile profile) {
 		this.NAME = profile.getPlayerName();
 		this.IMAGE = image;
 		this.COLOUR = Paint.valueOf(hexColour);
@@ -41,12 +42,12 @@ public class Player {
 		this.yLoc = yStart;
 		this.board = board;
 		this.PROFILE = profile;
+		this.backtracked = false;
 	}
 
 
 	/**
 	 * Returns the in-game name of the player token
-	 *
 	 * @return - The name of the player token
 	 */
 	public String getName() {
@@ -55,7 +56,6 @@ public class Player {
 
 	/**
 	 * Returns the image associated with this player
-	 *
 	 * @return - This player's image
 	 */
 	public Image getImage() {
@@ -64,7 +64,6 @@ public class Player {
 
 	/**
 	 * Returns the colour associated with this player
-	 *
 	 * @return - The colour as a paint value
 	 */
 	public Paint getColour() {
@@ -73,7 +72,6 @@ public class Player {
 
 	/**
 	 * Return all the cards this player is holding
-	 *
 	 * @return - The player's hand of tiles
 	 */
 	public ArrayList<Action> getHand() {
@@ -81,87 +79,124 @@ public class Player {
 	}
 
 	/**
-	 * @return
+	 * Returns the player's x coordinate
+	 * @return - The player's x coordinate
 	 */
 	public int getXLoc() {
 		return xLoc;
 	}
 
 	/**
-	 * @return
+	 * Returns the player's y coordinate
+	 * @return - The player's y coordinate
 	 */
 	public int getYLoc() {
 		return yLoc;
 	}
 
 	/**
+	 * sets backtracked to true
+	 */
+	public void toggleBacktracked(){
+		this.backtracked  = true;
+	}
+	/**
 	 * Adds a action tile to this player's hand
-	 *
 	 * @param action - The action tile to be added to the hand
 	 */
 	public void addActionTile(Action action) {
 		HAND.add(action);
 	}
 
-
+	/**
+	 * Moves the player to the tile he selects if possible
+	 * @param moveTo - The tile the player wants to move to
+	 */
 	public void movePlayer(Floor moveTo) {
 		if (possibleMoves().contains(moveTo)) {
-			this.currentTile = moveTo;
 			storePosi();
+			this.currentTile = moveTo;
 		} else {
 			throw new IndexOutOfBoundsException("ERROR: " + this.NAME + " cannot make this move");
 		}
 	}
 
-	public void storePosi() {
+	/**
+	 * Stores the last 3 positions, used to backtrack the player
+	 */
+	public void storePosi(){
 		lastPosiX[2] = lastPosiX[1];
 		lastPosiY[2] = lastPosiX[1];
+		lastPosiX[1] = lastPosiX[0];
+		lastPosiY[1] = lastPosiY[0];
+		lastPosiX[0] = xLoc;
+		lastPosiY[0] = yLoc;
 	}
 
+	/**
+	 * Returns all the possible moves a player can male from his current location
+	 * @return an arrayList of all possible tiles he can move to
+	 */
 	public ArrayList<Floor> possibleMoves() {
 		ArrayList<Floor> possibleMoves = new ArrayList<>();
-		if (isEastPossible()) {
-			possibleMoves.add(board.getTileAt(xLoc + 1, yLoc));
+		if (isEastPossible()){
+			possibleMoves.add(board.getTileAt(xLoc+1, yLoc));
 		}
 		if (isWestPossible()) {
-			possibleMoves.add(board.getTileAt(xLoc - 1, yLoc));
+			possibleMoves.add(board.getTileAt(xLoc-1, yLoc));
 		}
-		if (isNorthPossible()) {
-			possibleMoves.add(board.getTileAt(xLoc, yLoc - 1));
+		if (isNorthPossible()){
+			possibleMoves.add(board.getTileAt(xLoc, yLoc-1));
 		}
-		if (isSouthPossible()) {
-			possibleMoves.add(board.getTileAt(xLoc, yLoc + 1));
+		if (isSouthPossible()){
+			possibleMoves.add(board.getTileAt(xLoc, yLoc+1));
 		}
 		return possibleMoves;
 	}
 
+	/**
+	 * Checks if it is possible to move to the tile on the right
+	 * @return true if it is possible to move east, false otherwise
+	 */
 	public boolean isEastPossible() { //right
 		if (xLoc < board.getLength() - 1) {
-			return board.getTileAt(xLoc, yLoc).isWest() == true && board.getTileAt(xLoc + 1, yLoc).isEast() == true;
+			return board.getTileAt(xLoc,yLoc).isWest() == true && board.getTileAt(xLoc + 1, yLoc).isEast() == true;
 		} else {
 			return false;
 		}
 	}
 
-	public boolean isWestPossible() { //left
+	/**
+	 * Checks if it is possible to move to the tile on the left
+	 * @return true if it is possible to move west, false otherwise
+	 */
+	public boolean isWestPossible(){
 		if (xLoc > 0) {
-			return board.getTileAt(xLoc, yLoc).isEast() == true && board.getTileAt(xLoc - 1, yLoc).isWest() == true;
+			return board.getTileAt(xLoc,yLoc).isEast() == true && board.getTileAt(xLoc - 1, yLoc).isWest() == true;
 		} else {
 			return false;
 		}
 	}
 
-	public boolean isNorthPossible() {
+	/**
+	 * Checks if it is possible to move to the tile above
+	 * @return true if it is possible to move upwards, false otherwise
+	 */
+	public boolean isNorthPossible(){
 		if (yLoc > 0) {
-			return board.getTileAt(xLoc, yLoc).isSouth() == true && board.getTileAt(xLoc, yLoc - 1).isNorth() == true;
+			return board.getTileAt(xLoc,yLoc).isSouth() == true && board.getTileAt(xLoc, yLoc - 1).isNorth() == true;
 		} else {
 			return false;
 		}
 	}
 
-	public boolean isSouthPossible() {
+	/**
+	 * Checks if it is possible to move to the tile below
+	 * @return true if it is possible to move down, false otherwise
+	 */
+	public boolean isSouthPossible(){
 		if (yLoc < board.getHeight() - 1) {
-			return board.getTileAt(xLoc, yLoc).isNorth() == true && board.getTileAt(xLoc, yLoc + 1).isSouth() == true;
+			return board.getTileAt(xLoc,yLoc).isNorth() == true && board.getTileAt(xLoc, yLoc + 1).isSouth() == true;
 		} else {
 			return false;
 		}
@@ -169,18 +204,17 @@ public class Player {
 
 
 	/**
-	 * @param type
-	 * @return
+	 *
+	 * @param type - The type of the action he wants to play, if it is available
+	 * @return tempAction - The action card that is being played
 	 */
-	public Action playActionTile(String type) {
+	public Action playActionTile(String type) throws NullPointerException {
 		Action tempAction = null;
-		for (Action actionTile : HAND) {
-			if (actionTile.tileType.equalsIgnoreCase(type)) {
+		for (Action actionTile: HAND) {
+			if (actionTile.tileType.equalsIgnoreCase(type)) { //TODO fix this
 				tempAction = actionTile;
 			}
-			break;
 		}
-
 		if (tempAction == null) {
 			throw new NullPointerException("ERROR: " + this.NAME + " does not hold any " + type + " action tiles.");
 		} else {
@@ -190,48 +224,26 @@ public class Player {
 		return tempAction;
 	}
 
-
 	/**
-	 * @param board
+	 * This is going to be used when a backtrack tile is being played on them
+	 * @throws NullPointerException if the player cannot be backtracked
 	 */
-	public void moveN(Board board) {
-		if (yLoc == board.getHeight() - 1) {
-			throw new IndexOutOfBoundsException("ERROR: " + this.NAME + " is already at the top of the board.");
+	public void backtrack() throws NullPointerException{
+		if (!backtracked) {
+			if (!board.getTileAt(lastPosiX[1],lastPosiY[1]).getIsFire()) {
+				xLoc = lastPosiX[1];
+				yLoc = lastPosiY[1];
+			} else if (board.getTileAt(lastPosiX[1],lastPosiY[1]).getIsFire()) {
+				if (!board.getTileAt(lastPosiX[2],lastPosiY[2]).getIsFire()) {
+					xLoc = lastPosiX[2];
+					yLoc = lastPosiY[2];
+				} else if  (board.getTileAt(lastPosiX[2],lastPosiY[2]).getIsFire()) {
+					throw new NullPointerException("ERROR: " + this.NAME + " cannot be backtracked");
+				}
+			}
 		} else {
-			this.yLoc--;
-		}
-	}
-
-	/**
-	 * @param board
-	 */
-	public void moveE(Board board) {
-		if (xLoc == board.getLength() - 1) {
-			throw new IndexOutOfBoundsException("ERROR: " + this.NAME + " is already at the edge of the board.");
-		} else {
-			this.xLoc++;
-		}
-	}
-
-	/**
-	 *
-	 */
-	public void moveS() {
-		if (yLoc == 0) {
-			throw new IndexOutOfBoundsException("ERROR: " + this.NAME + " is already at the bottom of the board.");
-		} else {
-			this.yLoc++;
-		}
-	}
-
-	/**
-	 *
-	 */
-	public void moveW() {
-		if (xLoc == 0) {
-			throw new IndexOutOfBoundsException("ERROR: " + this.NAME + " is already at the edge of the board.");
-		} else {
-			this.xLoc--;
+			throw new NullPointerException("ERROR: " + this.NAME + " cannot be backtracked because " +
+					"player has been backtracked");
 		}
 	}
 
