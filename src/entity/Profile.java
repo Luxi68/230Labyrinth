379823402197133e13a181f60c1330954633af5f
SloCo-Albi <alberto.ortenzi@outlook.com>
2020-12-04 20:57:14 +0000

    Profile Selection works as intended and is Fully completed. Small update to leaderboard but will be completed tomorrow.

--- src/entity/Profile.java
+++ src/entity/Profile.java
@@ -1,8 +1,10 @@
 package entity;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Profile implements Comparable<Profile> {
     String playerName;
@@ -18,10 +20,15 @@ public class Profile implements Comparable<Profile> {
         File createdFile = new File(filename);
         FileWriter writing = new FileWriter(filename);
         writing.write(name);
-        writing.write(",");
-        writing.write("0");
-        writing.write(",");
-        writing.write("0");
+        writing.write(System.lineSeparator());
+        for (int i=1; i<=3 ; i++){
+            writing.write(Integer.toString(i));
+            writing.write(",");
+            writing.write("0");
+            writing.write(",");
+            writing.write("0");
+            writing.write(System.lineSeparator());
+        }
         writing.close();
     }
 
