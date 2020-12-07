2b95e535e31424c0050e077f10c24d81c013822f
Lucy <lucy.j.hu@gmail.com>
2020-12-07 01:18:29 +0000

    Fixed start screen Added images

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         URL url = getClass().getResource("/scene/StartScreen.fxml");
         Parent root = FXMLLoader.load(url);
+        root.setStyle("-fx-background-image: url('assets/mount.png');" + "-fx-background-size: cover");
         primaryStage.setTitle("The First Olympian");
         primaryStage.getIcons().add(new Image("assets/olympian.png"));
         primaryStage.setScene(new Scene(root, 640, 371));
