19af1a846dd91b451b4374187fa8f4fba086f8f6
P-Soulweave <Ev@13117032>
2020-12-07 01:49:14 +0000

    just made it so that save could work with it

--- src/entity/Action.java
+++ src/entity/Action.java
@@ -2,15 +2,17 @@ package entity;
 
 import javafx.scene.image.Image;
 
+import java.io.Serializable;
+
 /**
  * Action.java
  * This class defines the action tiles and declares what type of action tile is being used.
  *
  * @author Chris, Ryan and Nouran
  */
-public class Action extends Tile {
-
-    private final Type ACTION_TYPE;
+public class Action extends Tile implements Serializable {
+	
+	private final Type ACTION_TYPE;
 
     /**
      * The constructor initialising the type of action tile it is.
@@ -20,54 +22,33 @@ public class Action extends Tile {
     public Action(String tileType, Image image) {
         super(tileType, image);
 
-        switch (tileType) {
-            case ("fire"):
-                this.ACTION_TYPE = Type.fire;
-                break;
-            case ("ice"):
-                this.ACTION_TYPE = Type.ice;
-                break;
-            case ("doubleMove"):
-                this.ACTION_TYPE = Type.doubleMove;
-                break;
-            case ("backTrack"):
-                this.ACTION_TYPE = Type.backTrack;
-                break;
-            default: // TODO - Maybe throw an exception?
-                this.ACTION_TYPE = null;
-                System.out.println("Error: Tried to create action tile with invalid type");
-                break;
-        }
-    }
+		switch (tileType) {
+			case ("fire"):
+				this.ACTION_TYPE = Type.fire;
+				break;
+			case ("ice"):
+				this.ACTION_TYPE = Type.ice;
+				break;
+			case ("doubleMove"):
+				this.ACTION_TYPE = Type.doubleMove;
+				break;
+			case ("backTrack"):
+				this.ACTION_TYPE = Type.backTrack;
+				break;
+			default: // TODO - Maybe throw an exception?
+				this.ACTION_TYPE = null;
+				System.out.println("Error: Tried to create action tile with invalid type");
+				break;
+		}
+	}
+	
+	/**
+	 * 
+	 * The different types of action that can be used
+	 *
+	 */
+	enum Type {
+		fire, ice, doubleMove, backTrack
+	}
 
-    /**
-     * The different types of action that can be used
-     */
-    enum Type {
-        fire, ice, doubleMove, backTrack
-    }
-
-//	/**
-//	 * Method that uses action tiles which affect the floor tiles.
-//	 * @param tile - The tile which is being acted upon.
-//	 */
-//	public void useFireIce(Floor tile) {
-//		if (ACTION_TYPE.equals(Type.fire)) {
-//			tile.setIsFire(true);
-//		} else if (ACTION_TYPE.equals(Type.ice)) {
-//			tile.setIsIce(true);
-//		}
-//	}
-
-//	/**
-//	 * Method that uses action tiles which affect the player.
-//	 * @param player
-//	 */
-//	public void useBackDouble(Player player) {
-//		if (ACTION_TYPE.equals(Type.doubleMove)) {
-//			player.doubleMove();
-//		} else if (ACTION_TYPE.equals(Type.backTrack)){
-//			player.backTrack();
-//		}
-//	}
 }
